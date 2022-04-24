18290b4fb7f2d6395ecc7ea8a94a2668ed3cfe99
NiyantaPrasad <niyanta@zwayam.com>
2022-04-24 17:23:16 +0530

    controller spec added for user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /signup" do
-    it "returns http success" do
-      get "/users/signup"
-      expect(response).to have_http_status(:success)
+
+  FactoryBot.define do
+    factory :user do
+       trait :invalid_user do
+        first_name { 'Niyanta' }
+        last_name { 'Prasad' }
+        email_id { '' }
+        phone_no { '9957086386' }
+        password { 'pineapple' }
+      end
     end
   end
 
-  describe "GET /login" do
-    it "returns http success" do
-      get "/users/login"
-      expect(response).to have_http_status(:success)
+  describe "POST /signup" do
+    it "signup a user" do
+      headers = { "ACCEPT" => "application/json" } 
+      @invalid_user  = FactoryBot.create :user, :invalid_user
+      expect do
+        post "/signup", :params => { :first_name => @invalid_user.first_name, :last_name => @invalid_user.last_name, :email_id => @invalid_user.email_id, :phone_no =>  @invalid_user.phone_no, :password => @invalid_user.password}, :headers => headers
+      end.to change(User, :count).by(0)
     end
   end
 
+  describe "POST /login" do
+    it "login in as a user" do
+      headers = { "ACCEPT" => "application/json" }
+      expect do
+        post "/login", :params => { :email_id => @invalid_user.email_id, :password => @invalid_user.password}, :headers => headers
+      end.to change(User, :count).by(0)
+    end
+  end
 end
