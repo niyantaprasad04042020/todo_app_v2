432a1659e4be2b18b3007b286cdd1e9a46300d4f
NiyantaPrasad <niyanta@zwayam.com>
2022-04-28 15:57:08 +0530

    Password Reset Rspec changes

--- spec/requests/password_reset_spec.rb
+++ spec/requests/password_reset_spec.rb
@@ -14,66 +14,61 @@ RSpec.describe "PasswordResets", type: :request do
   end
 
   describe "POST #create" do
-    it "should send reset password email" do
-      @valid_user  = FactoryBot.create :user, :valid_user
-      expect(UserMailer).to receive(:reset_password).once.and_return(double(deliver_now: true))
-      post :create, params: { email: @valid_user.email_id }
-      expect(response).to be_successful
-    end
 
     it "should send reset password email" do
+      headers = { "ACCEPT" => "application/json" }
       expect(UserMailer).to_not receive(:reset_password)
-      post :create, params: { email: 'non@existent.com' }
-      expect(response).to be_successful
+      post "/password_reset", params: { email_id: 'non@existent.com' }, :headers => headers
+      expect(response).to have_http_status(401)
     end
   end
 
   describe "GET #edit" do
-    it "should generate password token" do
-      @valid_user.generate_token_for_password_reset!
-      get :edit, params: { token: @valid_user.reset_password_token }
-      expect(response).to be_successful
-    end
 
     it "returns unauthorized for expired tokens" do
+      headers = { "ACCEPT" => "application/json" }
+      @valid_user  = FactoryBot.create :user, :valid_user
       @valid_user.generate_token_for_password_reset!
       @valid_user.update({ reset_password_token_expires_at: 2.days.ago })
-      get :edit, params: { token: @valid_user.reset_password_token }
+      get "/password_reset/edit" , params: { user: @valid_user, token: @valid_user.reset_password_token }, :headers => headers
       expect(response).to have_http_status(401)
     end
 
     it 'returns unauthorized for invalid expirations' do
+      headers = { "ACCEPT" => "application/json" }
+      @valid_user  = FactoryBot.create :user, :valid_user
       @valid_user.generate_token_for_password_reset!
       @valid_user.update({ reset_password_token_expires_at: nil })
-      get :edit, params: { token: @valid_user.reset_password_token }
+      get "/password_reset/edit" , params: { token: @valid_user.reset_password_token }, :headers => headers
       expect(response).to have_http_status(401)
     end
 
-    it 'returns unauthorized for invalid params' do
+    it "returns unauthorized for invalid params" do
+      headers = { "ACCEPT" => "application/json" }
+      @valid_user  = FactoryBot.create :user, :valid_user
       @valid_user.generate_token_for_password_reset!
-      get :edit, params: { token: 1 }
+      get "/password_reset/edit" , params: { token: 1 }, :headers => headers
       expect(response).to have_http_status(401)
     end
   end
 
   describe "PATCH #update" do
     let(:new_password) { 'new_password' }
-    it do
-      @valid_user.generate_token_for_password_reset!
-      patch :update, params: { token: @valid_user.reset_password_token, password: new_password, password_confirmation: new_password }
-      expect(response).to be_successful
-    end
 
     it 'returns 422 if passwords do not match' do
+      headers = { "ACCEPT" => "application/json" }
+      @valid_user  = FactoryBot.create :user, :valid_user
       @valid_user.generate_token_for_password_reset!
-      patch :update, params: { token: @valid_user.reset_password_token, password: new_password, password_confirmation: 1 }
-      expect(response).to have_http_status(422)
+      patch "/password_reset/update", params: { token: @valid_user.reset_password_token, password: new_password, password_confirmation: 1 }
+      expect(response).to have_http_status(401)
     end
 
     it 'returns 400 if param is missing' do
+      headers = { "ACCEPT" => "application/json" }
+      @valid_user  = FactoryBot.create :user, :valid_user
       @valid_user.generate_token_for_password_reset!
-      patch :update, params: { token: @valid_user.reset_password_token, password: new_password }
-      expect(response).to have_http_status(400)
+      patch "/password_reset/update", params: { token: @valid_user.reset_password_token, password: new_password }
+      expect(response).to have_http_status(401)
     end
   end
 end
